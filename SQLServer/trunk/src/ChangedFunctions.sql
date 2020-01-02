71c1cca0957ff73fb76c17bdffe43b02caada81d
Simon Greener <simon@spdba.com.au>
2020-01-02 18:05:04 +1100

    2020-01-02 Simon Greener - New utility files not meant for distribution

---
+++ SQLServer/trunk/src/ChangedFunctions.sql
@@ -0,0 +1,13 @@
+USE DEVDB
+GO
+
+SELECT SCHEMA_NAME(schema_id) AS 'Schema',
+	   name AS 'Function Name',
+       TYPE,
+       type_desc AS 'Function Type', 
+       create_date AS 'Created Date',
+	   so.modify_date as 'Modify Date'
+  FROM sys.objects as so
+ WHERE SCHEMA_NAME(schema_id) in ('dbo','lrs','cogo')
+   and type in ('FN', 'IF', 'FN', 'AF', 'FS', 'FT', 'P')
+ ORDER BY so.modify_date desc;
